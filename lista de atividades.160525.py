2bf52b2b6e5ea0bdba090e2672452a24f42599be
LeonardoLoes471 <4553682400@estudante.sed.sc.gov.br>
2025-05-16 23:53:56 -0300

    Add files via upload

---
+++ lista de atividades.160525.py
@@ -0,0 +1,92 @@
+#1 Peça ao usuário para digitar 5 números e mostre a soma deles ao final.
+
+#num1 = int (input('digite o primeiro número: ')) 
+#num2 = int (input('digite o segundo número: ')) 
+#num3 = int (input('digite o terceiro número: ')) 
+#num4 = int (input('digite o quarto número: ')) 
+#num5 = int (input('digite o quinto número: ')) 
+
+#soma = num1 + num2 + num3 + num4 + num5
+
+#print(f"A soma é: {soma}")
+
+#2 Peça ao usuário para digitar 4 números e mostre qual é o maior e qual é o menor.
+
+#num1 = input(print('digite o primeiro número: ')) 
+#num2 = input(print('digite o segundo número: ')) 
+#num3 = input(print('digite o terceiro número: ')) 
+#num4 = input(print('digite o quarto número: ')) 
+
+#numeros = [num1, num2, num3, num4]
+
+#maior= max(numeros)
+#menor= min(numeros)
+
+#print(f"O maior número é {maior}")
+#print(f"O menor número é {menor}")
+
+#3 Peça ao usuário uma palavra e mostre quantas vogais ela tem.
+
+#palavra = input("Digite uma palavra: ")
+#vogais = "a, e, i, o, u, A, E, I, O, U"  
+#contador_vogais = 0  
+
+#for letra in palavra:  
+#    if letra in vogais:  
+#        contador_vogais += 1  
+
+#print(f"A palavra '{palavra}' tem {contador_vogais} vogais.")
+
+#4 Peça ao usuário para digitar 6 números e mostre apenas os números pares digitados.
+
+#print ('Peça ao usuario para digitar 6 numeros e mostre apenas os numeros pares')
+
+#num1 = int(input('digite o primeiro numero: '))
+#num2 = int(input('digite o segundo numero: '))
+#num3 = int(input('digite o terceiro numero: '))
+#num4 = int(input('digite o quarto numero: '))
+#num5 = int(input('digite o quinto numero: '))
+#num6 = int(input('digite o sexto numero: '))
+
+#listadepares = []
+
+#listadenumeros = [num1, num2, num3, num4, num5, num6]
+
+#for numero in listadenumeros:
+#    if numero % 2 ==0:
+#        listadepares.append(numero)
+
+#if len(listadepares) != 0:
+#    print(f"Os numeros pares digitados foram:{listadepares}")
+#else:
+#    print("A liesta não tem nenhum numero par!")
+
+#5 Solicite as notas de 4 provas e mostre a média.
+
+#nota1 = float(input('digite a primeira nota:'))
+#nota2 = float(input('digite a segunda nota:'))
+#nota3 = float(input('digite a terceira nota:'))
+#nota4 = float(input('digite a quarta nota:'))
+
+#media = (nota1 + nota2 + nota3 + nota4) /4
+
+#print (f'Media das nota nota1, nota2, nota3, nota4  é: {media}')
+
+#6 Peça ao usuário um número e mostre a tabuada desse número de 1 a 10.
+
+
+#7 Peça um número N ao usuário e mostre todos os números de 1 até N.
+
+
+#8 Peça ao usuário uma palavra e mostre ela ao contrário.
+
+#9 Peça um número ao usuário e diga se ele é múltiplo de 3.
+
+#10 Peça ao usuário para digitar 3 nomes e mostre todos eles em ordem alfabética.
+    
+
+
+
+
+
+
